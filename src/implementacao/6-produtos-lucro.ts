6ef7d80ac0bac578c60467d7be32178e6a6eeab3
Jhamyllie <milli.santana2017@gmail.com>
2025-05-12 09:36:43 -0300

    fix: Adiciona correção para pegar os produtos wue não derem lucro

--- src/implementacao/6-produtos-lucro.ts
+++ src/implementacao/6-produtos-lucro.ts
@@ -3,35 +3,46 @@ function calcularLucros(produtos: {nome: string, custo: number, precoDevenda: nu
     let lucroMenorQue10: number = 0;
     let lucroEntre10E30: number = 0;
     let lucroMaiorQue30: number = 0;
+    let produtoSemLucro: number = 0
 
     for(let produto in produtos){
         let lucro: number = produtos[produto].precoDevenda - produtos[produto].custo;
         let porcentagemDeLucro: number = (lucro / produtos[produto].custo) * 100;
 
-        if (porcentagemDeLucro < 10) {
-            lucroMenorQue10++
-        }else if(porcentagemDeLucro >= 10 && porcentagemDeLucro <= 30){
-            lucroEntre10E30++
-        }else{
-            lucroMaiorQue30++
+        if (lucro < 0) {    
+            produtoSemLucro++       
+        } else {
+            if (porcentagemDeLucro < 10) {
+                lucroMenorQue10++
+
+            }else if(porcentagemDeLucro >= 10 && porcentagemDeLucro <= 30){
+                lucroEntre10E30++
+                
+            }else if(porcentagemDeLucro > 30){
+                lucroMaiorQue30++
+            }
         }
+        
     }
+
     return `
         Quantidade de produtos que têm menos de 10% de lucro: ${lucroMenorQue10}
         Quantidade de produtos que têm entre 10% e 30% de lucro: ${lucroEntre10E30}
-        Quantiadde de produtos que têm mais de 30% de lucro: ${lucroEntre10E30}
+        Quantidade de produtos que têm mais de 30% de lucro: ${lucroEntre10E30}
+        Quantidade de produtos que não deram lucro: ${produtoSemLucro}
     `
 
 }
 
 
-// console.log(calcularLucros([
-//     {nome:"Anel", custo:20, precoDevenda:21},
-//     {nome:"Sapato", custo: 200, precoDevenda:400},
-//     {nome:"Sapato", custo: 200, precoDevenda:240},
-//     {nome:"Sapato", custo: 200, precoDevenda:250},
-//     {nome:"Sapato", custo:200, precoDevenda:260},
-//     {nome:"Sorvete", custo:3, precoDevenda:16},
-//     {nome:"Bola", custo:50, precoDevenda:80}
-// ]));
+console.log(calcularLucros([
+    {nome:"Anel", custo:20, precoDevenda:21},
+    {nome:"Sapato", custo: 200, precoDevenda:400},
+    {nome:"Sapato", custo: 200, precoDevenda:240},
+    {nome:"Sapato", custo: 200, precoDevenda:250},
+    {nome:"Sapato", custo:200, precoDevenda:260},
+    {nome:"Sorvete", custo:3, precoDevenda:16},
+    {nome:"Bola", custo:50, precoDevenda:80},
+    {nome:"Chinela", custo:20, precoDevenda:15}
+]));
 
